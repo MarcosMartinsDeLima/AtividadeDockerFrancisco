7578c24946ebb301e52584430051e03abe12411a
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-05-05 22:20:44 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Escola.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudComDocker.Models
+{
+    public class Escola
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
+    }
+}
